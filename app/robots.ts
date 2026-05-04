4272a46d9763dc7ba16c918f606977304cd069a5
asuma-labs <asuma.my.id@gmail.com>
2026-05-05 06:59:36 +0700

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,8 @@
-import { MetadataRoute } from 'next';
+// app/robots.ts
+import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = 'https://asuma.my.id';
+  const baseUrl = 'https://asuma.my.id'
 
   return {
     rules: {
@@ -10,16 +11,15 @@ export default function robots(): MetadataRoute.Robots {
       disallow: [
         '/_next/',
         '/api/',
+        '/auth/',
         '/login',
-        '/register',
         '/dashboard/',
-        '/settings/',
-        '/private/',
         '/*/settings',
-        '/*/edit',
-        '/*/private',
+        '/*/bots/',
+        '/*/profile',
+        '/private/',
       ],
     },
     sitemap: `${baseUrl}/sitemap.xml`,
-  };
+  }
 }
